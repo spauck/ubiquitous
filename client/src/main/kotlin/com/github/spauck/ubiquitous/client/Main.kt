ee6e5aa9ff8366f7dd4b3040c237a27123c86b3b
Simon Pauck <simon.pauck@gmail.com>
2021-01-23 17:58:20 +0000

    Add main file in client lib

---
+++ client/src/main/kotlin/com/github/spauck/ubiquitous/client/Main.kt
@@ -0,0 +1,15 @@
+package com.github.spauck.ubiquitous.client
+
+fun main()
+{
+  val server = Server(InMemoryHttpJson()).host(port = 9180)
+  onShutdown(server::shutdown)
+}
+
+private fun onShutdown(run: () -> Unit)
+{
+  Runtime.getRuntime().addShutdownHook(object : Thread()
+  {
+    override fun run() = run()
+  })
+}
